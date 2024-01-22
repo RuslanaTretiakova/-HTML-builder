8ec7c0db8f020ce9decac7285edeeeb454fc77b2
Ruslana <tretiakovaruslana7@gmail.com>
2024-01-22 14:13:59 +0100

    feat: implement 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const folderPath = path.resolve(__dirname, 'files');
+const destPath = path.resolve(__dirname, 'files-copy');
+
+async function copyDirectory() {
+  try {
+    await fs.mkdir(destPath, { recursive: true });
+    const files = await fs.readdir(folderPath);
+
+    for (const file of files) {
+      const sourceFilePath = path.resolve(folderPath, file);
+      const destFilePath = path.resolve(destPath, file);
+
+      await fs.copyFile(sourceFilePath, destFilePath);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyDirectory();
