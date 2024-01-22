2f22c4ac9541374323a636920d9296e9c3498c41
Ruslana <tretiakovaruslana7@gmail.com>
2024-01-22 12:12:17 +0100

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.resolve(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.resolve(folderPath, file.name);
+        const fileExt = path.parse(file.name).ext.slice(1);
+
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const fileSize = (stats.size / 1024).toFixed(2) + 'kb';
+
+            process.stdout.write(`${file.name} ${fileExt} ${fileSize} \n`);
+          }
+        });
+      }
+    });
+  }
+});
